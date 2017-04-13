e0658d139a293358b5b0f0caefe99d23f38a01c3
Madson Paulo <madson-paulo@hotmail.com>
2017-04-13 00:34:23 -0300

    Finished TabelaConsultaImp.java

--- UFAL_BD/src/br/ufal/aracomp/ufalbd/externo/metamodel/TabelaConsultaImp.java
+++ UFAL_BD/src/br/ufal/aracomp/ufalbd/externo/metamodel/TabelaConsultaImp.java
@@ -1,38 +1,50 @@
 package br.ufal.aracomp.ufalbd.externo.metamodel;
 
 import java.util.ArrayList;
+import br.ufal.aracomp.ufalbd.conceitual.metamodel.Dado;
 
-import br.ufal.aracomp.ufalbd.conceitual.metamodel.Coluna;
-
+/**
+ * Tabela de Consulta
+ * 
+ * @author Madson
+ *
+ */
 public class TabelaConsultaImp implements TabelaConsulta {
-	private ArrayList<Coluna> columns;
+	private ArrayList<ArrayList<Dado>> arrayTuplas;
+
+	public TabelaConsultaImp(ArrayList<ArrayList<Dado>> arrayTuplas) {
+		this.arrayTuplas = new ArrayList<ArrayList<Dado>>();
+		this.arrayTuplas = arrayTuplas;
+	}
 
 	@Override
 	public String obterNomeColuna(int ordemColuna) {
-		return columns.get(ordemColuna).obterNomeColuna();
+		return arrayTuplas.get(0).get(ordemColuna).obterColuna().obterNomeColuna();
 	}
 
 	@Override
 	public String obterDadoColuna(int ordemColuna, int ordemTupla) {
-		// TODO Auto-generated method stub
-		return null;
+		return arrayTuplas.get(ordemTupla).get(ordemColuna).obterValorDado().toString();
 	}
 
 	@Override
 	public String obterDadoColuna(String nomeColuna, int ordemTupla) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < arrayTuplas.get(ordemTupla).size(); i++) {
+			if (arrayTuplas.get(ordemTupla).get(i).obterColuna().obterNomeColuna().equals(nomeColuna)) {
+				return arrayTuplas.get(ordemTupla).get(i).obterValorDado().toString();
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public int obterNumeroTuplas() {
-		// TODO Auto-generated method stub
-		return 0;
+		return arrayTuplas.size();
 	}
 
 	@Override
 	public int obterNumeroColulas() {
-		return columns.size();
+		return arrayTuplas.get(0).size();
 	}
 
 }
