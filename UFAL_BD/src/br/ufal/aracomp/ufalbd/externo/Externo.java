cdb315c9c3d5b42ef1560bc85045661bafa7a2ab
reuelcaetano <reuelshow@hotmail.com>
2017-04-25 11:33:37 -0300

    Add files via upload

--- UFAL_BD/src/br/ufal/aracomp/ufalbd/externo/Externo.java
+++ UFAL_BD/src/br/ufal/aracomp/ufalbd/externo/Externo.java
@@ -1,32 +1,97 @@
 package br.ufal.aracomp.ufalbd.externo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import br.ufal.aracomp.ufalbd.conceitual.Conceitual;
+import br.ufal.aracomp.ufalbd.conceitual.exception.BancoInvalidoException;
+import br.ufal.aracomp.ufalbd.conceitual.exception.CondicaoInvalidaException;
+import br.ufal.aracomp.ufalbd.conceitual.exception.ErroInsercaoException;
+import br.ufal.aracomp.ufalbd.conceitual.exception.TabelaInvalidaException;
 import br.ufal.aracomp.ufalbd.conceitual.metamodel.Coluna;
-import br.ufal.aracomp.ufalbd.conceitual.metamodel.Condicao;
+import br.ufal.aracomp.ufalbd.conceitual.metamodel.ColunaImp;
+import br.ufal.aracomp.ufalbd.conceitual.metamodel.CondicaoSimples;
 import br.ufal.aracomp.ufalbd.conceitual.metamodel.Dado;
-import br.ufal.aracomp.ufalbd.conceitual.metamodel.Tabela;
+import br.ufal.aracomp.ufalbd.conceitual.metamodel.DadoImp;
+import br.ufal.aracomp.ufalbd.conceitual.metamodel.TabelaImp;
 import br.ufal.aracomp.ufalbd.externo.exception.InstrucaoMalFormadaException;
 import br.ufal.aracomp.ufalbd.externo.metamodel.TabelaConsulta;
 
 public class Externo implements IExterno{
 
-	@Override
-	public boolean executarDDL(String instrucao) throws InstrucaoMalFormadaException {
+	Conceitual c = new Conceitual();
+	
+	public String[] separaComandos(String instrucao){
 		String caixaBaixa = instrucao.toLowerCase();
 		String[] comandos = caixaBaixa.split(" ");
-		int numComandos = comandos.length;
-		if (numComandos == 2) {
-			if (comandos[0] == "criar_bd") {
-				Conceitual.criarBanco(comandos[1]);
-			} else if (comandos[0] == "excluir_bd"){
-				Conceitual.apagarBanco(comandos[1]);
+		return comandos;
+	}
+	public void procExcluirTupla(String arg1, String arg2, CondicaoSimples arg3) {
+		try {
+			c.excluirTupla(arg1, arg2, arg3);
+		} catch (BancoInvalidoException e) {
+			e.printStackTrace();
+		} catch (TabelaInvalidaException e) {
+			e.printStackTrace();
+		} catch (CondicaoInvalidaException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public boolean executarDDL(String instrucao) throws InstrucaoMalFormadaException {
+		String[] comandos = separaComandos(instrucao);
+		if (comandos.length == 2) {
+			switch(comandos[0]){
+			case "construir_bd":
+				try {
+					c.criarBanco(comandos[1]);
+				} catch (BancoInvalidoException e) {
+					e.printStackTrace();}
+			case "destruir_bd":
+				try {
+					c.apagarBanco(comandos[1]);
+				} catch (BancoInvalidoException e) {
+					e.printStackTrace();}
 			}
-		} else if (numComandos == 3) {
-			if (comandos[0] == "criar_tb") {
-				Conceitual.criarTabela(comandos[1], Tabela null); // Falta implementar a classe Tabela
-			} else if (comandos[0] == "excluir_tb") {
-				Conceitual.apagarTabela(comandos[1], comandos[2]);
+			
+		} else if (comandos.length == 3) {
+			switch(comandos[0]){
+			case "construir_tb":
+				String nomeTabela = comandos[2].split(Pattern.quote("("))[0];
+				String descricao = comandos[2].split(Pattern.quote("("))[1].split(Pattern.quote(")"))[0];
+				String[] colunas = descricao.split(",");
+				
+				List<Coluna> colunaList = new ArrayList<>();
+				for (int i = 0; i < colunas.length; i++) {
+					if (colunas[i].split(":")[1] == "texto") {
+						colunaList.add(new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 1));
+					} else if (colunas[i].split(":")[1] == "numero") {
+						colunaList.add(new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 2));
+					} else if (colunas[i].split(":")[1] == "data_hora") {
+						colunaList.add(new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 3));
+					} else if (colunas[i].split(":")[1] == "bytes") {
+						colunaList.add(new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 4));
+					}
+				}
+				TabelaImp tabela = new TabelaImp(nomeTabela, colunaList);
+				try {
+					c.criarTabela(comandos[1], tabela);
+				} catch (BancoInvalidoException e) {
+					e.printStackTrace();
+				} catch (TabelaInvalidaException e) {
+					e.printStackTrace();
+				}
+			
+			case "destruir_tb":
+				try {
+					c.apagarTabela(comandos[1], comandos[2]);
+				} catch (BancoInvalidoException e) {
+					e.printStackTrace();
+				} catch (TabelaInvalidaException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		return false;
@@ -34,26 +99,75 @@ public class Externo implements IExterno{
 
 	@Override
 	public int executarDMLInstrucao(String instrucao) throws InstrucaoMalFormadaException {
-		String caixaBaixa = instrucao.toLowerCase();
-		String[] comandos = caixaBaixa.split(" ");
-		int numComandos = comandos.length;
-		if (numComandos == 4) {
-			if (comandos[0] == "inserir") {
-				Conceitual.inserirTupla(comandos[1], comandos[2], List<Dado> null); // Tratando
-			} else if (comandos[0] == "excluir") {
-				Conceitual.excluirTupla(comandos[1], comandos[2, Condicao null]); // Tratando
+		String[] comandos = separaComandos(instrucao);
+		if (comandos.length == 3) {
+			
+			String nomeTabela = comandos[2].split(Pattern.quote("("))[0];
+			String descricao = comandos[2].split(Pattern.quote("("))[1].split(Pattern.quote(")"))[0];
+			String[] colunas = descricao.split(",");
+			
+			List<Dado> dadoList = new ArrayList<>();
+			for (int i = 0; i < colunas.length; i++) {
+				if (colunas[i].split(":")[1] == "texto") {
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 1);
+					Dado e = new DadoImp(col, colunas[i].split(":")[2]);
+					dadoList.add(e);
+				} else if (colunas[i].split(":")[1] == "numero"){
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 2);
+					Dado e = new DadoImp(col, colunas[i].split(":")[2]);
+					dadoList.add(e);
+				} else if (colunas[i].split(":")[1] == "data_hora"){
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 3);
+					Dado e = new DadoImp(col, colunas[i].split(":")[2]);
+					dadoList.add(e);
+				} else if (colunas[i].split(":")[1] == "bytes"){
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[i].split(":")[0], 4);
+					Dado e = new DadoImp(col, colunas[i].split(":")[2]);
+					dadoList.add(e);
+				}
 			}
+			
+			switch(comandos[0]) {
+			case "adicionar":
+				try {
+					c.inserirTupla(comandos[1], nomeTabela, dadoList);
+				} catch (BancoInvalidoException e) {
+					e.printStackTrace();
+				} catch (TabelaInvalidaException e) {
+					e.printStackTrace();
+				} catch (ErroInsercaoException e) {
+					e.printStackTrace();
+				}
+			case "remover":
+				if (colunas[0].split(":")[1] == "texto") {
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[0].split(":")[0], 1);
+					CondicaoSimples cond = new CondicaoSimples(col, 1, colunas[0].split(":")[2]);
+					procExcluirTupla(comandos[1], nomeTabela, cond);
+
+				} else if (colunas[0].split(":")[1] == "numero") {
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[0].split(":")[0], 2);
+					CondicaoSimples cond = new CondicaoSimples(col, 1, colunas[0].split(":")[2]);
+					procExcluirTupla(comandos[1], nomeTabela, cond);
+
+				} else if (colunas[0].split(":")[1] == "data_hora") {
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[0].split(":")[0], 3);
+					CondicaoSimples cond = new CondicaoSimples(col, 1, colunas[0].split(":")[2]);
+					procExcluirTupla(comandos[1], nomeTabela, cond);
+
+				} else if (colunas[0].split(":")[1] == "bytes") {
+					Coluna col = new ColunaImp(comandos[1], nomeTabela, colunas[0].split(":")[0], 4);
+					CondicaoSimples cond = new CondicaoSimples(col, 1, colunas[0].split(":")[2]);
+					procExcluirTupla(comandos[1], nomeTabela, cond);
+				}}
 		}
 		return 0;
 	}
 
 	@Override
 	public TabelaConsulta executarDMLConsulta(String instrucao) throws InstrucaoMalFormadaException {
-		String caixaBaixa = instrucao.toLowerCase();
-		String[] comandos = caixaBaixa.split(" ");
-		int numComandos = comandos.length;
-		if (numComandos == 5) {
-			Conceitual.consultar(comandos[1], List<String> null, List<Coluna> null, Condicao null); // Tratando
+		String[] comandos = separaComandos(instrucao);
+		if (comandos.length == 5) {
+			
 		}
 		return null;
 	}
---
+++ UFAL_BD/src/br/ufal/aracomp/ufalbd/externo/Tutorial.java
@@ -0,0 +1,23 @@
+package br.ufal.aracomp.ufalbd.externo;
+/**
+ * 
+ * TUTORIAL DE INSTRUCOES DDL E DML PARA FAZER USO DO BANCO DE DADOS
+ * 
+ * 
+ * DDL ------------------------------ \
+ *  
+ * CRIAR BANCO DE DADOS = "CONSTRUIR_BD nomeBanco"
+ * APAGAR BANCO DE DADOS = "DESTRUIR_BD nomeBanco"
+ * 
+ * CRIAR TABELA = "CONSTRUIR_TB nomeBanco nomeTabela(coluna1:tipoDado,coluna2:tipoDado,...)"
+ * APAGAR TABELA = "DESTRUIR_TB nomeBanco nomeTabela"
+ * 
+ * 
+ * DML ------------------------------ \
+ * 
+ * INSERIR TUPLA = "ADICIONAR nomeBanco nomeTabela(coluna1:tipoDado:valor,coluna2:tipoDado:valor,...)"
+ * EXCLUIR TUPLA = "REMOVER nomeBanco nomeTabela(coluna:op:valor)"
+ * 
+ * CONSULTAR = "BUSCAR nomeBanco (tabela1,tabela2,...) (coluna1,coluna2,...) coluna:op:valor"
+ * 
+ * */
